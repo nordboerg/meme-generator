52c3fd2c202735cdf8067b7b1d66c07b3c031eb1
Norbert Gresku <norbert.gresku@1account.net>
2023-10-15 18:18:42 +0100

    styled() with custom props - typed

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,4 +1,6 @@
 import type { ReactNode } from 'react'
+import type { ContainerProps } from '@mui/material'
+import { styled } from '@mui/system'
 import CssBaseline from '@mui/material/CssBaseline'
 import Container from '@mui/material/Container'
 import GlobalStyles from '@mui/material/GlobalStyles'
@@ -7,24 +9,27 @@ interface Props {
   children: ReactNode
 }
 
+type ContainerWithProps = ContainerProps & { isMobile: boolean }
+
 export const isMobile = window.innerWidth <= 425
+
+const CustomContainer = styled(Container, {
+  shouldForwardProp: (prop) => prop !== 'isMobile',
+})<ContainerWithProps>(({ isMobile }) => ({
+  marginTop: isMobile ? 0 : 3,
+  paddingTop: 1,
+  backgroundColor: '#faf0e6',
+  borderRadius: isMobile ? 0 : 2,
+}))
 
 const Layout = ({ children }: Props) => {
   return (
     <>
       <CssBaseline />
       <GlobalStyles styles={{ body: { backgroundColor: '#5f9ea0' } }} />
-      <Container
-        maxWidth="md"
-        sx={{
-          marginTop: isMobile ? 0 : 3,
-          paddingTop: 1,
-          backgroundColor: '#faf0e6',
-          borderRadius: isMobile ? 0 : 2,
-        }}
-      >
+      <CustomContainer maxWidth="md" isMobile={isMobile}>
         {children}
-      </Container>
+      </CustomContainer>
     </>
   )
 }
