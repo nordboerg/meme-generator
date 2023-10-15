90c0cfa0d87d5c3d297bac4115b0023cc86832d9
Norbert Gresku <norbert.gresku@1account.net>
2023-10-15 18:14:38 +0100

    import type

--- src/components/DownloadDialog/DownloadDialog.tsx
+++ src/components/DownloadDialog/DownloadDialog.tsx
@@ -5,7 +5,7 @@ import DialogContent from '@mui/material/DialogContent'
 import DialogActions from '@mui/material/DialogActions'
 import IconButton from '@mui/material/IconButton'
 import CloseIcon from '@mui/icons-material/Close'
-import { Dispatch, SetStateAction } from 'react'
+import type { Dispatch, SetStateAction } from 'react'
 
 interface Props {
   memeUrl: string
