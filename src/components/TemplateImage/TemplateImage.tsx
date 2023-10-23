873598d07e26630b1319c94ef17e2db5ec6f2635
Norbert Gresku <norbert.gresku@1account.net>
2023-10-23 18:03:36 +0100

    add lazy template image loading

--- src/components/TemplateImage/TemplateImage.tsx
+++ src/components/TemplateImage/TemplateImage.tsx
@@ -1,14 +1,21 @@
 import { useNavigate } from 'react-router-dom'
+import { useInView } from 'react-intersection-observer'
 import ImageListItem from '@mui/material/ImageListItem'
 import { MemeTemplate } from '../../interfaces/Meme'
 import { useContext } from 'react'
 import { MemeContext } from '../../context/MemeContext'
+import { isMobile } from '../Layout/Layout'
 
 interface Props {
   template: MemeTemplate
 }
+
 const TemplateImage = ({ template }: Props) => {
   const { setSelectedMeme } = useContext(MemeContext)
+  const { ref, inView } = useInView({
+    rootMargin: '0px 0px 400px 0px',
+    triggerOnce: true,
+  })
   const navigate = useNavigate()
   const onImageClick = () => {
     setSelectedMeme(template)
@@ -17,15 +24,17 @@ const TemplateImage = ({ template }: Props) => {
 
   return (
     <ImageListItem
+      ref={ref}
       onClick={onImageClick}
       sx={{
+        minHeight: isMobile ? 100 : 200,
         cursor: 'pointer',
         '&:hover': {
           opacity: 0.9,
         },
       }}
     >
-      <img src={template.url} alt={template.name} loading="lazy" />
+      <img src={inView ? template.url : ''} alt={template.name} loading="lazy" />
     </ImageListItem>
   )
 }
